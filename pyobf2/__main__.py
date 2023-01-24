2777b119db0f669b51ab8180b3a1cc1895255626
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2023-01-24 06:56:08 +0100

    We do a little cock fighting

--- pyobf2/__main__.py
+++ pyobf2/__main__.py
@@ -1,4 +1,10 @@
+import pathlib
+import os
+import sys
+
 if __name__ == "__main__":
+    if os.environ.get("ADD_PATH"):
+        sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
     from pyobf2 import main
 
     main()
--- pyobf2/lib/transformers/packPyz.py
+++ pyobf2/lib/transformers/packPyz.py
@@ -1,11 +1,47 @@
+import atexit
+import inspect
 import os.path
 import pathlib
 import shutil
+import string
 import tempfile
+import textwrap
 import zipfile
 
-from ..cfg import ConfigValue
-from . import Transformer
+from . import *
+
+
+def _lhandler(key_passphrase, mainfile):
+    import Crypto.PublicKey.RSA as RSA
+    from Crypto.Cipher import PKCS1_OAEP as PKCS1_OAEP
+    from zipfile import ZipFile as ZipFile, ZIP_DEFLATED as ZIP_DEFLATED
+    from io import BytesIO as BytesIO
+    import tempfile as tmpf
+    import sys as sys
+    import os.path as pth
+    import pathlib as pthlib
+
+    root = pthlib.Path(__file__).parent
+
+    root_dir_a = pthlib.Path(tmpf.mkdtemp())
+    this_dir = root_dir_a.joinpath("b")
+
+    with ZipFile(root, "r", ZIP_DEFLATED) as f:
+        f.extractall(this_dir)
+
+    with open(this_dir.joinpath("0"), "rb") as f, open(this_dir.joinpath("id.rsa"), "rb") as f1:
+        enc_content = f.read()
+        key_bytes = f1.read()
+    key = RSA.import_key(key_bytes, key_passphrase)
+    decrypted_dir = root_dir_a.joinpath("a")
+    res = PKCS1_OAEP.new(key).decrypt(enc_content)
+    with ZipFile(BytesIO(res), "r", ZIP_DEFLATED) as f:
+        f.extractall(decrypted_dir)
+    sys.path.insert(0, str(decrypted_dir))  # add to search path
+    atexit.register(lambda v: __import__("shutil").rmtree(root_dir_a))
+
+    with open(pth.join(decrypted_dir, mainfile), "r") as f:
+        exec(f.read())
 
 
 class PackInPyz(Transformer):
@@ -18,6 +54,7 @@ class PackInPyz(Transformer):
                 "The file to start when the .pyz is started. File will be renamed to __main__.py inside the .pyz",
                 "__main__.py",
             ),
+            encrypt=ConfigValue("Encrypts all contents of the .pyz using a random RSA256 key", True),
         )
 
     def transform_output(self, output_location: pathlib.Path, all_files: list[pathlib.Path]) -> list[pathlib.Path]:
@@ -51,6 +88,36 @@ class PackInPyz(Transformer):
             ignore_errors=False,
             onerror=None,
         )
+        if self.config["encrypt"].value:
+            import Crypto.PublicKey.RSA as RSA
+            from Crypto.Cipher import PKCS1_OAEP
+
+            k = RSA.generate(2048)
+            passphr = "".join(random.choices(string.printable, k=16))
+            enc_key = k.export_key("PEM", passphr, 8, "scryptAndAES128-CBC")
+            with open(out_path, "rb") as f:
+                orig_bytes = f.read()
+            pk = PKCS1_OAEP.new(k)
+
+            launcher_bc = _lhandler.__code__
+
+            gs = inspect.getsource(launcher_bc)
+            gs = textwrap.dedent(gs)
+            generted_ast = ast.parse(gs)
+            generted_ast = optimize_ast(generted_ast)
+
+            gn_ast = Module(
+                body=[
+                    *generted_ast.body,
+                    Expr(Call(func=Name("m0", Load()), args=[Constant(passphr), Constant("__main__.py")], keywords=[])),
+                ],
+                type_ignores=[],
+            )
+            with zipfile.ZipFile(out_path, "w", zipfile.ZIP_DEFLATED, compresslevel=9) as f:
+                f.writestr("0", pk.encrypt(orig_bytes))
+                f.writestr("id.rsa", enc_key)
+                f.writestr("__main__.py", ast.unparse(ast.fix_missing_locations(gn_ast)))
+
         output_location.mkdir(parents=True, exist_ok=True)
         fin = output_location.joinpath("archive.pyz")
         with open(fin, "wb") as outfile, open(out_path, "rb") as infile:
